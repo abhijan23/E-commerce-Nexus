43e09f80f178ad466002ef6bfba3d74ec5454da4
Abhijan Mallick <mallickabhijan23@gmail.com>
2025-06-30 22:16:25 +0530

    Initial commit

---
+++ backend/ecommerce/src/main/java/com/nexus/ecommerce/model/Product.java
@@ -0,0 +1,25 @@
+package com.nexus.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+    private String imageUrl;
+}
