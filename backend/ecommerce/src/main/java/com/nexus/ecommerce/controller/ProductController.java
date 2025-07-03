5fcd0f5646dffa94e7112758db6a0b79b26b6ea6
Abhijan Mallick <mallickabhijan23@gmail.com>
2025-07-03 22:59:46 +0530

    updated Commit

--- backend/ecommerce/src/main/java/com/nexus/ecommerce/controller/ProductController.java
+++ backend/ecommerce/src/main/java/com/nexus/ecommerce/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.nexus.ecommerce.controller;
 
+import com.nexus.ecommerce.model.Product;
 import com.nexus.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -11,5 +15,21 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product saveProduct = productService.createProduct(product);
+        return new ResponseEntity<>(saveProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        Product product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
 }
