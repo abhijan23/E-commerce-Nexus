e305386011035a0e1a63f2424c1caefbe455f1fd
Abhijan Mallick <mallickabhijan23@gmail.com>
2025-07-27 19:17:01 +0530

    Updated Backend

--- backend/ecommerce/src/main/java/com/nexus/ecommerce/config/SecurityConfig.java
+++ backend/ecommerce/src/main/java/com/nexus/ecommerce/config/SecurityConfig.java
@@ -1,15 +1,55 @@
 package com.nexus.ecommerce.config;
 
+import com.nexus.ecommerce.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable()) //1. Diable CSRF
+                .authorizeHttpRequests(auth -> auth //2. Define authorization rules
+                        .requestMatchers("/api/auth/**").permitAll() //Allow all requests to auth endpoints
+                        .anyRequest().authenticated() // Secure all other requests
+                )
+                .sessionManagement(session -> session // 3. Set session management to stateless
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider()); // 4. Specify the authentication provider
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(customUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
