8c940c821f369f4efb90edde9821ec79d78e9756
Abhijan Mallick <mallickabhijan23@gmail.com>
2025-07-28 09:11:35 +0530

    updated backend

--- backend/ecommerce/src/main/java/com/nexus/ecommerce/service/JwtService.java
+++ backend/ecommerce/src/main/java/com/nexus/ecommerce/service/JwtService.java
@@ -1,7 +1,18 @@
 package com.nexus.ecommerce.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKeyString;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+    
 }
